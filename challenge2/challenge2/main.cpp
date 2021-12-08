be072fd2650b79538f87ac18a879d5a1741923b0
John Davies <jd.other@hotmail.com>
2021-12-08 21:05:16 +0000

    challenge2 initial commit

---
+++ challenge2/challenge2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  challenge2
+//
+//  Created on 08/12/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
