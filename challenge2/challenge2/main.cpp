b3c4d8bcce1ce3e96e0a47e0c1f5b69fa55ceeac
John Davies <jd.other@hotmail.com>
2021-12-08 21:49:11 +0000

    initial comments

--- challenge2/challenge2/main.cpp
+++ challenge2/challenge2/main.cpp
@@ -7,8 +7,15 @@
 
 #include <iostream>
 
+using namespace std;
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    // Handle invalid input and format the input strings correctly
+    // Convert both input strings (plaintext and key) to binary.
+    // Use the XOR bitwise operation to find the output (ciphertext). The bitwise operator can be applied to the entire string (no need to XOR one bit at a time).
+    // Convert the binary output into hex.
+    // Output this to std out.
+    
+    
     return 0;
 }
