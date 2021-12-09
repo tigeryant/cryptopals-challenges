7f2c52f14c5a1160c3fd81a29b60f83807a26735
John Davies <jd.other@hotmail.com>
2021-12-09 12:26:29 +0000

    more comments, initial declarations

--- challenge2/challenge2/main.cpp
+++ challenge2/challenge2/main.cpp
@@ -6,8 +6,25 @@
 //
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
+
+// helper functions
+
+// function - convertToBinary - takes a string, returns a vector of bools
+// string (hex) --> char vector (hex) --> bool vector (binary)
+
+// function - rejectInvalidInput
+
+// function - formatInput
+
+// function - xor
+// takes two arguments of type vector <bool>
+// xors them and returns a vector <bool>
+
+// function - convertToHex
+// takes a vector <bool>, converts it to hex and returns a vector <char>
 
 int main(int argc, const char * argv[]) {
     // Handle invalid input and format the input strings correctly
@@ -15,6 +32,40 @@ int main(int argc, const char * argv[]) {
     // Use the XOR bitwise operation to find the output (ciphertext). The bitwise operator can be applied to the entire string (no need to XOR one bit at a time).
     // Convert the binary output into hex.
     // Output this to std out.
+    
+    // declarations
+    string hexPlainText;
+    vector<char> hexPlainTextVector; // maybe this could be declared locally within convertToBinary (use a generic hexInputVector for any input)
+    vector<bool> binPlainTextVector;
+    
+    
+    string hexKey;
+    vector<bool> binKeyVector;
+    
+    vector<bool> binCipherText;
+    vector<char> hexCipherText;
+    
+    cout << "Enter the plaintext in hexadecimal format: ";
+    cin >> hexPlainText;
+    
+    // call rejectInvalidInput
+    // call formatInput
+    // call convertToBinary, assign it to vector<bool> binPlainTextVector
+    
+    cout << "Enter the key in hexadecimal format: ";
+    cin >> hexKey;
+    
+    // call rejectInvalidInput
+    // call formatInput
+    // call convertToBinary, assign it to vector<bool> binKeyVector
+    
+    // call xor, passing binPlainTextVector and binKeyVector
+    // assign the result to binCipherText
+    
+    // call convertToHex, pass binCipherText
+    // assign the result to hexCipherText
+    
+    // output the vector <char>
     
     
     return 0;
